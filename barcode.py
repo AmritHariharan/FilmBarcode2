293f5420b5546f29f004be18712c5545ae024b57
AmritHariharan <stuka241@gmail.com>
2017-05-06 12:29:57 +0800

    Added rolling average, now counts per actual pixel. Still getting float to int errors...

--- barcode.py
+++ barcode.py
@@ -1,5 +1,29 @@
 import imageio
 import numpy as np
+import math
+from queue import *
+
+
+# -------------------- HELPER FUNCTIONS -------------------- #
+
+def get_average(q):
+	total = 0
+	count = 0
+	while not q.empty():
+		total = total + q.get()
+		count = count + 1
+	if total < 1:
+		print("ITS 0 LMAO GG")
+		print(total)
+	return int(total / count)
+
+def pop_all(q):
+	while not q.empty():
+		temp = q.get()
+	return q
+
+
+# -------------------------- MAIN -------------------------- #
 
 # Get filename from user
 #print "What file do you want to convert?"
@@ -13,17 +37,29 @@ reader = imageio.get_reader('stay.mp4')
 length = reader.get_length()
 width = 2560
 height = 1600
+q_length = length / width # NOTE: THIS CANNOT BE 0
+q = Queue(q_length) # queue for rolling average with max size q_length
+px_count = 0
 
 wallpaper = np.zeros((height, width))
-sq_img = np.zeros((length, length))
 
+# Iterating through rows
 for i, im in enumerate(reader):
-	#print('Mean of frame %i is %1.1f' % (i, im.mean()))
-	mean = im.mean()
-	for c in range(length):
-		sq_img[c][i] = mean
-
+	# Adding to the rolling average
+	q.put(im.mean())
+	# Once the rolling average has reached its limit, add the
+	# average to the image and reset
+	if q.full():
+		# Iterating through one column
+		pixel = get_average(q)
+		for c in range(height):
+			wallpaper[c][px_count] = pixel
+		print(px_count)
+		px_count = px_count + 1
+		# Emptying the queue
+		q = pop_all(q) # there must be a better way to do this...
+		#print(q.qsize())
 
 # Writing final image
 writer = imageio.get_writer('test.png')
-writer.append_data(sq_img)
+writer.append_data(wallpaper)
